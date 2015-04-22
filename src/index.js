5c77412830da27d7b94d1ea20578993e709b655d
Michael Paulson <mpaulson@netflix.com>
2015-04-22 06:39:10 -0700

    version bump

--- src/index.js
+++ src/index.js
@@ -20,7 +20,9 @@ parser.fromPathsOrPathValues = function(paths, ext) {
 
         // is the path a path value with a string value.
         else if (typeof paths[i].path === 'string') {
-            out[i].path = parser(paths[i].path, ext);
+            out[i] = {
+                path: parser(paths[i].path, ext), value: paths[i].value
+            };
         }
 
         // just copy it over.
